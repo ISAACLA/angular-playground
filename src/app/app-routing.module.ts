18d9009d735bd0c81d43f070432080bef74e50d7
ISAACLA <yanbinla@gmail.com>
2022-02-07 22:37:20 -0800

    package initial set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { TempMainComponent } from './temp-main/temp-main.component';
+import { UsersModule } from './users/users.module';
+import { AlbumsModule } from './albums/albums.module';
+import { PostsModule } from './posts/posts.module';
+import { TodosModule } from './todos/todos.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TempMainComponent,
+  },
+  {
+    path: 'users',
+    loadChildren: () => UsersModule,
+  },
+  {
+    path: 'albums',
+    loadChildren: () => AlbumsModule,
+  },
+  {
+    path: 'posts',
+    loadChildren: () => PostsModule,
+  },
+  {
+    path: 'todos',
+    loadChildren: () => TodosModule,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersModule } from './users/users.module';
+import { TempMainComponent } from './temp-main/temp-main.component';
+import { UsersState } from './users/state/users.state';
+import { NgxsModule } from '@ngxs/store';
+import { AlbumsModule } from './albums/albums.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { TodosModule } from './todos/todos.module';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TempMainComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxsModule.forRoot([], { developmentMode: true }),
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    UsersModule,
+    AlbumsModule,
+    TodosModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
